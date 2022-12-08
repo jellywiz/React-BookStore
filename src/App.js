2eb43ec84c722447065c7f93490d87307114d6b1
jellywiz <muzaffer.dler@gmail.com>
2022-12-08 14:40:12 +0300

    Add react routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Books from './components/pages/Books';
+import Catigoties from './components/pages/Catigories';
 
 function App() {
   return (
     <div className="App">
-      <h1>BookStore</h1>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Catigoties />} />
+        </Routes>
+      </div>
     </div>
   );
 }
